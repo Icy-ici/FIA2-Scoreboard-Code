7f3ff2a7ebab5b962ef780b759128d3911accf0f
Icy-ici <rwhee30@outlook.com>
2023-05-20 16:23:31 +1000

    basically finishing

--- main_window.py
+++ main_window.py
@@ -1,40 +1,80 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QMainWindow
-from scoreboard_ui import Ui_MainWindow
+from PyQt6.uic import loadUi
+from PyQt6.QtCore import QTimer
 
-class MainWindow:
+class MainWindow(QMainWindow):
     def __init__(self):
+        super(MainWindow, self).__init__()
+        loadUi("FIA2_Design_1.ui", self)  
 
-        ## --- setup UI elements --- ##
-        self.main_win = QMainWindow()
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self.main_win)
+         # Connect buttons to their respective functions
+        self.resetScoresButton.clicked.connect(self.reset_scores)
+        self.resetTimerButton.clicked.connect(self.reset_timer)
+        self.resetAllButton.clicked.connect(self.reset_all)
+        self.team1Add1Button.clicked.connect(lambda: self.change_score(1, 1))
+        self.team1Add2Button.clicked.connect(lambda: self.change_score(1, 2))
+        self.team1SubtractButton.clicked.connect(lambda: self.change_score(1, -1))
+        self.team2Add1Button.clicked.connect(lambda: self.change_score(2, 1))
+        self.team2Add2Button.clicked.connect(lambda: self.change_score(2, 2))
+        self.team2SubtractButton.clicked.connect(lambda: self.change_score(2, -1))
+        self.addMinuteButton.clicked.connect(self.add_minute)
+        self.addSecondButton.clicked.connect(self.add_second)
+        self.startButton.clicked.connect(self.start_clock)
+        self.pauseButton.clicked.connect(self.pause_clock)
 
-        ## --- Initialise scoreboard variables --- ##
-        self.Team1_name = ""
-        self.Team2_name = ""
-        self.Team1_logo = ""
-        self.Team2_logo = ""
-        self.Team1_score = 0
-        self.Team2_score = 0
-        self.Presets = 0
-        ## --- Initialise the UI with starting values --- ###
-        
+        # Initialize variables
+        self.team1Score = 0
+        self.team2Score = 0
+        self.clock = QTimer()
+        self.clock.timeout.connect(self.update_time)
+        self.timeInSeconds = 0
 
+    def reset_scores(self):
+        self.team1Score = 0
+        self.team2Score = 0
+        self.team1ScoreLabel.display(self.team1Score)
+        self.team2ScoreLabel.display(self.team2Score)
 
+    def reset_timer(self):
+        self.timeInSeconds = 0
+        self.update_time()
 
-    def show(self):
-        self.main_win.show()
+    def reset_all(self):
+        self.reset_scores()
+        self.reset_timer()
+      
 
+    def change_score(self, team, points):
+        if team == 1:
+            self.team1Score += points
+            self.team1ScoreLabel.display(self.team1Score)
+        elif team == 2:
+            self.team2Score += points
+            self.team2ScoreLabel.display(self.team2Score)
 
+    def update_time(self):
+        minutes = self.timeInSeconds // 60
+        seconds = self.timeInSeconds % 60
+        self.clockLabel.display(f"{minutes:02d}:{seconds:02d}")
+        self.timeInSeconds += 1
+
+    def add_minute(self):
+        self.timeInSeconds += 60
+        self.update_time()
+
+    def add_second(self):
+        self.timeInSeconds += 1
+        self.update_time()
+
+    def start_clock(self):
+        self.clock.start(1000)  # Update every second
+
+    def pause_clock(self):
+        self.clock.stop()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    main_win = MainWindow()
-    main_win.show()
+    main_window = MainWindow()
+    main_window.show()
     sys.exit(app.exec())
-
-
-   
-
-
